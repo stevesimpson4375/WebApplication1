7a14f0b463df04097407d6c6e938604cb966cdd6
steve simpson <stevesimpson4375@gmail.com>
2017-08-13 20:43:41 -0400

    Initial Comit

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -8,11 +8,13 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             return View();
         }
 
+        [RequireHttps]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
